0576e8673023d5922c4a054acc718726741f9077
Matt Phillips <mattphillips1016@gmail.com>
2017-07-10 23:50:35 +0100

    WIP Add poc component builder

---
+++ src/index.js
@@ -0,0 +1,96 @@
+import fse from 'fs-extra';
+
+import inquirer from 'inquirer';
+
+import pure from './templates/pure-component';
+import stateful from './templates/stateful-component';
+import css from './templates/styles';
+import test from './templates/test.js';
+/*
+Name
+
+Component type:
+Stateful | Stateless | HOC | stateless HOC?
+
+CSS type:
+CSS | SASS | LESS | Styled-components | etc
+
+Test suffix:
+.test.js | .spec.js
+
+Component filetype:
+.js | .jsx
+
+Component naming convention:
+index | Name
+*/
+console.log('Time to build: a -> HTML');
+
+var questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Component name?'
+  },
+  {
+    type: 'input',
+    name: 'directory',
+    message: 'Directory to create component in?'
+  },
+  {
+    type: 'list',
+    name: 'componentType',
+    message: 'Component type?',
+    choices: ['Stateful', 'Stateless', 'HOC']
+  },
+  {
+    type: 'list',
+    name: 'cssType',
+    message: 'CSS type?',
+    choices: ['CSS', 'SASS', 'LESS', 'styled-components'],
+    default: 'CSS'
+  },
+  {
+    type: 'list',
+    name: 'testSuffix',
+    message: 'Test Suffix?',
+    choices: ['test', 'spec']
+  },
+  {
+    type: 'list',
+    name: 'componentFiletype',
+    message: 'Component Filetype?',
+    choices: ['js', 'jsx']
+  },
+  {
+    type: 'list',
+    name: 'namingConvention',
+    message: 'File naming convention?',
+    choices: ['index.js|x', 'ComponentName.js|x']
+  }
+];
+
+inquirer.prompt(questions).then(function(answers) {
+  const componentFilepath =
+    answers.namingConvention === 'index.js|x'
+      ? `index.${answers.componentFiletype}`
+      : `${answers.name}.${answers.componentFiletype}`;
+  fse.outputFile(`${answers.directory}/${answers.name}/${componentFilepath}`, pure(answers), function(err) {
+    if (err) return console.error(err);
+    console.log('Component file created!');
+  });
+
+  const testFilepath =
+    answers.namingConvention === 'index.js|x'
+      ? `index.${answers.testSuffix}.${answers.componentFiletype}`
+      : `${answers.name}.${answers.testSuffix}.${answers.componentFiletype}`;
+  fse.outputFile(`${answers.directory}/${answers.name}/${testFilepath}`, test(answers), function(err) {
+    if (err) return console.error(err);
+    console.log('Test file created!');
+  });
+
+  fse.outputFile(`${answers.directory}/${answers.name}/styles.${answers.cssType}`, css(answers), function(err) {
+    if (err) return console.error(err);
+    console.log('Component stylesheet created!');
+  });
+});
